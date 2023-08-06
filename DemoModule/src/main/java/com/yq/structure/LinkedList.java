0ed4cc672f8bdb0a032487797637dd46d5a4085b
yuqing <yuqingzcmu@163.com>
2023-08-06 10:48:24 +0800

    链表

--- DemoModule/src/main/java/com/yq/structure/LinkedList.java
+++ DemoModule/src/main/java/com/yq/structure/LinkedList.java
@@ -131,19 +131,5 @@ public class LinkedList<E> {
         }
     }
 
-    public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<>();
-        // 添加元素
-        list.add("a");
-        list.addFirst("b");
-        list.addLast("c");
-        // 打印列表
-        list.printLinkList();
-        // 头插元素
-        list.addFirst("d");
-        // 删除元素
-        list.remove("b");
-        // 打印列表
-        list.printLinkList();
-    }
+
 }
