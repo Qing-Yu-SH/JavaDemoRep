e667191722768d721610471290c338e930c97b8b
yuqing <yuqingzcmu@163.com>
2023-08-08 10:47:06 +0800

    平衡二叉树

--- DemoModule/src/main/java/com/yq/structure/test/TreeTest.java
+++ DemoModule/src/main/java/com/yq/structure/test/TreeTest.java
@@ -1,5 +1,6 @@
 package com.yq.structure.test;
 
+import com.yq.structure.tree.AVLTree;
 import com.yq.structure.tree.BinarySearchTree;
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +28,28 @@ public class TreeTest {
         Integer del2 = tree.elementList.get(2);
         tree.delete(del2);
         System.out.println("删除节点：" + del2);
+        System.out.println(tree);
+    }
+
+    @Test
+    public void test_AVLTree(){
+        AVLTree tree = new AVLTree();
+        for (int i = 0; i < 30; i++) {
+            tree.insert(new Random().nextInt(100));
+        }
+        System.out.println(tree);
+    }
+
+    @Test
+    public void test_AVLTree2(){
+        int[] nums = new int[]{61,3,34,82,1,75,56,65,87,18,3,96,53,50,42,24,69,11,95,69,1,1,84,22,5,70,28,55,38,92};
+        AVLTree tree = new AVLTree();
+        for(int num: nums){
+            tree.insert(num);
+        }
+        System.out.println(tree);
+
+        tree.delete(53);
         System.out.println(tree);
     }
 
