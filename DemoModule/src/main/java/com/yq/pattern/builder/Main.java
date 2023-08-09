ff7a6021841429cc09584df01045868eb57db96f
sumAll <20211510143@stu.sspu.edu.cn>
2023-08-09 10:30:28 +0800

    建造者模式

---
+++ DemoModule/src/main/java/com/yq/pattern/builder/Main.java
@@ -0,0 +1,21 @@
+package com.yq.pattern.builder;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: Yuqing
+ * @create: 2023-08-09 10:21
+ **/
+
+/**
+ * 建造者模式
+ * 建造者模式所完成的内容就是通过将多个简单对象通过⼀步步的组装构建出⼀个复杂对象的过程
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
