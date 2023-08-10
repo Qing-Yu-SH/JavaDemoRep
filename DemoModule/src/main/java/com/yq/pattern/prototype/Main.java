d71eb59df6f616451c2a488af4c118acddcaf07f
sumAll <20211510143@stu.sspu.edu.cn>
2023-08-10 10:47:29 +0800

    原型模式

---
+++ DemoModule/src/main/java/com/yq/pattern/prototype/Main.java
@@ -0,0 +1,18 @@
+package com.yq.pattern.prototype;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: Yuqing
+ * @create: 2023-08-10 10:34
+ **/
+
+/**
+ * 原型模式
+ * 原型模式主要解决的问题就是创建重复对象，⽽这部分对象内容本身⽐较复杂，⽣成过程可能从库或者RPC接⼝中获取数据，耗时较⻓，因此采⽤克隆的⽅式节省时间
+ *
+ */
+public class Main {
+
+
+}
