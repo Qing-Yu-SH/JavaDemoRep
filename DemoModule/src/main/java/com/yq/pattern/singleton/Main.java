eda960959a933ac70a3dd757933aaed6d70eaa10
sumAll <20211510143@stu.sspu.edu.cn>
2023-08-11 09:50:58 +0800

    单例模式

---
+++ DemoModule/src/main/java/com/yq/pattern/singleton/Main.java
@@ -0,0 +1,18 @@
+package com.yq.pattern.singleton;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: Yuqing
+ * @create: 2023-08-11 09:46
+ **/
+
+/**
+ * 单例模式
+ * 单例模式主要解决的是，⼀个全局使⽤的类频繁的创建和消费，从⽽提升整体的代码的性能
+ *
+ */
+public class Main {
+
+
+}
