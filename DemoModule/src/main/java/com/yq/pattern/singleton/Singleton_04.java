a8671a71c54148172376235fdac3656885d69859
sumAll <20211510143@stu.sspu.edu.cn>
2023-08-11 10:12:59 +0800

    单例模式 - 使用类的内部类（线程安全）

---
+++ DemoModule/src/main/java/com/yq/pattern/singleton/Singleton_04.java
@@ -0,0 +1,25 @@
+package com.yq.pattern.singleton;
+
+/**
+ * @program: JavaDemoRep
+ * @description: 使用类的内部类 - 线程安全
+ *               既保证了线程安全又保证了懒加载，同时不会因为加锁的⽅式耗费性能
+ *               静态内部类的实例化过程是延迟加载的，从而实现了懒加载
+ *               静态内部类的初始化由 Java 虚拟机保证线程安全，从而实现了线程安全
+ * @author: Yuqing
+ * @create: 2023-08-11 10:06
+ **/
+public class Singleton_04 {
+
+    private static class SingletonHolder{
+        private static Singleton_04 instance = new Singleton_04();
+    }
+
+    private Singleton_04(){
+    }
+
+    public static Singleton_04 getInstance(){
+        return SingletonHolder.instance;
+    }
+
+}
