338d08536cdf385cb4126bf91ac96ac8946d1302
sumAll <20211510143@stu.sspu.edu.cn>
2023-08-14 11:02:42 +0800

    享元模式 - Redis

---
+++ DemoModule/src/main/java/com/yq/pattern/flyweight/util/RedisUtils.java
@@ -0,0 +1,31 @@
+package com.yq.pattern.flyweight.util;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: Yuqing
+ * @create: 2023-08-14 10:44
+ **/
+public class RedisUtils {
+
+    private ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+    private AtomicInteger stock = new AtomicInteger(0);
+
+    public RedisUtils() {
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            // 模拟库存消耗
+            stock.addAndGet(1);
+        }, 0, 100000, TimeUnit.MICROSECONDS);
+    }
+
+    public int getStockUsed() {
+        return stock.get();
+    }
+
+}
