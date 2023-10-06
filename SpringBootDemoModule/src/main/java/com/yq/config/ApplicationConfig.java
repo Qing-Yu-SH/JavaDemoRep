4b8c7c3359b098c2965fcdde821d9c8795f4a54b
sumAll <20211510143@stu.sspu.edu.cn>
2023-10-06 19:23:57 +0800

    SpringBoot Demo - 配置类

--- SpringBootDemoModule/src/main/java/com/yq/config/ApplicationConfig.java
+++ SpringBootDemoModule/src/main/java/com/yq/config/ApplicationConfig.java
@@ -2,14 +2,19 @@ package com.yq.config;
 
 import com.yq.hook.filter.ZFilter;
 import com.yq.hook.servlet.HealthServlet2;
+import com.yq.util.RedisClient;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import javax.servlet.Filter;
 import javax.servlet.Servlet;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @program: JavaDemoRep
@@ -19,6 +24,12 @@ import java.util.Arrays;
  **/
 @Configuration
 public class ApplicationConfig {
+
+    // 通过 Spring Boot 的自动配置机制进行自动注入；Spring Boot 会根据配置，自动创建一个 RedisTemplate 实例，并注入构造方法
+    public ApplicationConfig(RedisTemplate<String, String> redisTemplate) {
+       RedisClient.register(redisTemplate);
+    }
+
 
     // 注册 Filter 方式 2
     @Bean
@@ -40,5 +51,17 @@ public class ApplicationConfig {
         registrationBean.setServlet(new HealthServlet2());
         return registrationBean;
     }
+
+    @Bean(name = "freeMarkerConfigurer")
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setDefaultEncoding("UTF-8");
+        configurer.setTemplateLoaderPath("classpath:");
+        Map<String, Object> variables = new HashMap<>(1<<1);
+        variables.put("xml_escape","fmXmlEscape");
+        configurer.setFreemarkerVariables(variables);
+        return configurer;
+    }
+
 
 }
