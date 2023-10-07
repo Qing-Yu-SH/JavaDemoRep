3d2e711603a3b6c48e94a5ec9910bec0df74ee3e
sumAll <20211510143@stu.sspu.edu.cn>
2023-10-07 15:18:46 +0800

    SpringBoot Demo - 字符串常量

---
+++ SpringBootDemoModule/src/main/java/com/yq/common/CommonConstants.java
@@ -0,0 +1,18 @@
+package com.yq.common;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: Yuqing
+ * @create: 2023-10-06 21:42
+ **/
+public class CommonConstants {
+
+    /**
+     * 消息队列
+     */
+    public static String EXCHANGE_NAME_DIRECT = "direct.exchange";
+    public static String QUERE_KEY_PRAISE = "praise";
+    public static String QUERE_NAME_PRAISE = "quere.praise";
+
+}
