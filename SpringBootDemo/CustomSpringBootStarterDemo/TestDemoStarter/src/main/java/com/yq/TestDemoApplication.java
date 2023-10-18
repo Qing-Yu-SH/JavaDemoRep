3f1945837e398ebde2e31e6ee6e3fb82002b0eb9
sumAll <20211510143@stu.sspu.edu.cn>
2023-10-18 16:32:48 +0800

    自定义 spring-boot-starter

---
+++ SpringBootDemo/CustomSpringBootStarterDemo/TestDemoStarter/src/main/java/com/yq/TestDemoApplication.java
@@ -0,0 +1,19 @@
+package com.yq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: Yuqing
+ * @create: 2023-10-18 15:47
+ **/
+@SpringBootApplication
+public class TestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDemoApplication.class);
+    }
+
+}
