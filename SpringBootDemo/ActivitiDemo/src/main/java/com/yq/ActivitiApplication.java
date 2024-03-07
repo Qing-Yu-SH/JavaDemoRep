db2824061106ecf0e50b6ad5159c5f307f836bf3
sumAll <20211510143@stu.sspu.edu.cn>
2024-03-07 09:54:10 +0800

    Activiti 学习项目初始化

--- SpringBootDemo/ActivitiDemo/src/main/java/com/yq/ActivitiApplication.java
+++ SpringBootDemo/ActivitiDemo/src/main/java/com/yq/ActivitiApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @program: JavaDemoRep
  * @description: Activiti 学习项目
- *               原链接：https://blog.csdn.net/weixin_49561506/article/details/130791619
+ *               Activiti实战——Springboot 整合 Activiti：https://blog.csdn.net/weixin_49561506/article/details/130791619
+ *               Activiti 的基本使用：https://blog.csdn.net/Relievedz/article/details/130244209
+ *               数据库表变化：https://juejin.cn/post/7311633278497554472
  * @author: Yuqing
  * @create: 2024-03-07 09:39
  **/
