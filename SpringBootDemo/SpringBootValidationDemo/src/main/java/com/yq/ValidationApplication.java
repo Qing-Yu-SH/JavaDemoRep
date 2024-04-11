2e55d64912c0a458947c2411a414b06a76527a29
sumAll <20211510143@stu.sspu.edu.cn>
2024-04-11 16:30:08 +0800

    SpringBoot 参数校验项目初始化

---
+++ SpringBootDemo/SpringBootValidationDemo/src/main/java/com/yq/ValidationApplication.java
@@ -0,0 +1,19 @@
+package com.yq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: sumAll
+ * @create: 2024-04-11 16:27
+ **/
+@SpringBootApplication
+public class ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationApplication.class, args);
+    }
+
+}
