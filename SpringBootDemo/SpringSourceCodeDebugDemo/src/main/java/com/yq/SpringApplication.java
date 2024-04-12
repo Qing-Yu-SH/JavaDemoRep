532b1d38675efe221e36f7d838b2267619d48501
sumAll <20211510143@stu.sspu.edu.cn>
2024-04-12 17:54:23 +0800

    项目初始化

---
+++ SpringBootDemo/SpringSourceCodeDebugDemo/src/main/java/com/yq/SpringApplication.java
@@ -0,0 +1,18 @@
+package com.yq;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: sumAll
+ * @create: 2024-04-12 11:55
+ **/
+@SpringBootApplication
+public class SpringApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+    }
+
+}
