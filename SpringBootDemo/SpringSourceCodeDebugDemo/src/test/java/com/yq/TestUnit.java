0a18313724e5065ba6024d7794b99925613d8a4e
sumAll <20211510143@stu.sspu.edu.cn>
2024-04-15 15:27:49 +0800

    添加测试单元

---
+++ SpringBootDemo/SpringSourceCodeDebugDemo/src/test/java/com/yq/TestUnit.java
@@ -0,0 +1,24 @@
+package com.yq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: sumAll
+ * @create: 2024-04-15 15:25
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestUnit {
+
+    @Test
+    public void test_lifecycle(){
+        System.out.println("test_lifecycle");
+    }
+
+
+}
