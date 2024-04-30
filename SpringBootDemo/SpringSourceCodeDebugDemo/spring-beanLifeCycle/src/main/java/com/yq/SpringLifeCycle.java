c3e05a3672743defa342fb7303452e3693dce93e
sumAll <20211510143@stu.sspu.edu.cn>
2024-04-30 14:30:28 +0800

    Spring bean生命周期 场景

---
+++ SpringBootDemo/SpringSourceCodeDebugDemo/spring-beanLifeCycle/src/main/java/com/yq/SpringLifeCycle.java
@@ -0,0 +1,19 @@
+package com.yq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: sumAll
+ * @create: 2024-04-30 14:23
+ **/
+@SpringBootApplication
+public class SpringLifeCycle {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLifeCycle.class, args);
+    }
+
+}
