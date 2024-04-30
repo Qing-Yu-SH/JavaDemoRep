e2b942d4f0fe0b112fce4fdf1c97de8ba3468692
sumAll <20211510143@stu.sspu.edu.cn>
2024-04-30 13:56:03 +0800

    Spring 三级缓存解决循环依赖 场景

---
+++ SpringBootDemo/SpringSourceCodeDebugDemo/spring-circularDependency/src/main/java/com/yq/SpringCircularDependencyApplication.java
@@ -0,0 +1,19 @@
+package com.yq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: sumAll
+ * @create: 2024-04-30 13:45
+ **/
+@SpringBootApplication
+public class SpringCircularDependencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCircularDependencyApplication.class, args);
+    }
+
+}
