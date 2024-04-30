adfa41a488193a39e5fefe1b5ddf700418bd1e07
sumAll <20211510143@stu.sspu.edu.cn>
2024-04-30 14:37:40 +0800

    Spring AOP 场景

---
+++ SpringBootDemo/SpringSourceCodeDebugDemo/spring-aop/src/main/java/com/yq/aop/AopService.java
@@ -0,0 +1,18 @@
+package com.yq.aop;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: sumAll
+ * @create: 2024-04-23 11:11
+ **/
+@Service
+public class AopService {
+
+    public void think(){
+        System.out.println("思考..");
+    }
+
+}
