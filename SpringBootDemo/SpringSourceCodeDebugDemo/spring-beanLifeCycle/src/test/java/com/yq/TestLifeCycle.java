ad8eb4a0a46f0168b38aeb9d27eed784711348df
sumAll <20211510143@stu.sspu.edu.cn>
2024-04-30 14:30:47 +0800

    Spring bean生命周期 场景

---
+++ SpringBootDemo/SpringSourceCodeDebugDemo/spring-beanLifeCycle/src/test/java/com/yq/TestLifeCycle.java
@@ -0,0 +1,30 @@
+package com.yq;
+
+import com.yq.lifecycle.LifecycleBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: sumAll
+ * @create: 2024-04-30 14:28
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestLifeCycle {
+
+    @Test
+    public void test_lifecycle(){
+        // debug bean 生命周期；断点打在这里，从这里进入 refresh() 方法，进入创建 bean 的流程
+        ApplicationContext context =new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        LifecycleBean lifecycleBean = (LifecycleBean) context.getBean("lifecycleBean");
+        lifecycleBean.work();
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+
+}
