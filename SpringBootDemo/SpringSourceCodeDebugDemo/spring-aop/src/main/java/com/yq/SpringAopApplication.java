7b26b70e1b8a1797d04d1baa6ee3f909c55473c4
sumAll <20211510143@stu.sspu.edu.cn>
2024-04-30 14:37:57 +0800

    Spring AOP 场景

---
+++ SpringBootDemo/SpringSourceCodeDebugDemo/spring-aop/src/main/java/com/yq/SpringAopApplication.java
@@ -0,0 +1,19 @@
+package com.yq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: JavaDemoRep
+ * @description:
+ * @author: sumAll
+ * @create: 2024-04-30 14:31
+ **/
+@SpringBootApplication
+public class SpringAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopApplication.class, args);
+    }
+
+}
